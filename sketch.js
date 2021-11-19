f9dfd52080f348a4f2e3975f4894fafb36a54ea1
Nimit17062008 <92149487+Nimit17062008@users.noreply.github.com>
2021-11-19 20:32:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
-
+var box1,box2;
 function setup() 
 {
   createCanvas(400, 400);
+  box1 = new Box(250,250,60,70,-5,-5)
 }
 
 function draw() 
 {
   background(220);
+ box1.show()
+ box1.moveUp()
+ box1.move()
 
 }
 
